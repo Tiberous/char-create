68b67999019b736df9c4092352ac0a6bb01d0fee
Tiberous <33761052+Tiberous@users.noreply.github.com>
2017-12-16 11:56:14 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express')
-app.set('view engine', 'ejs')
+
 const app = express()
 
 app.get('/', function (req, res) {
   // NEW CODE
   res.render('index');
 })
-
+app.set('view engine', 'ejs')
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
