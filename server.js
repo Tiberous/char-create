1c88210a24d359d1ec148c20f86b22559b15262a
Tiberous <33761052+Tiberous@users.noreply.github.com>
2017-12-16 20:30:04 -0600

    Practice on express routing

--- server.js
+++ server.js
@@ -1,37 +1,7 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const request = require('request');
-const app = express()
+var express = require('express')
+var app = express()
 
-const apiKey = '*****************';
-
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs')
-
+// respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
-  res.render('index', {weather: null, error: null});
-})
-
-app.post('/', function (req, res) {
-  let city = req.body.city;
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
-
-  request(url, function (err, response, body) {
-    if(err){
-      res.render('index', {weather: null, error: 'Error, please try again'});
-    } else {
-      let weather = JSON.parse(body)
-      if(weather.main == undefined){
-        res.render('index', {weather: null, error: 'Error, please try again'});
-      } else {
-        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-        res.render('index', {weather: weatherText, error: null});
-      }
-    }
-  });
-})
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  res.send('hello world')
 })
