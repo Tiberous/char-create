e047e32ea8ef08cbc7283c1949259c23e7b3a60b
Tiberous <33761052+Tiberous@users.noreply.github.com>
2017-12-16 20:57:43 -0600

    a

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ var express = require('express')
 var app = express()
 
 // respond with "hello world" when a GET request is made to the homepage
+var server = http.createServer(function (request, response) {  
+  response.writeHead(200, {"Content-Type": "text/html"});
+
 app.get('/', function (req, res) {
   res.send('hello world')
 })
-server.listen(9000);
+server.listen(9000)})
+
