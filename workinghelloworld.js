908092503a757c4ed2a4f5a0f476582725c7b6a3
Tiberous <33761052+Tiberous@users.noreply.github.com>
2017-12-27 15:31:51 -0600

    Updating to pull to local host

---
+++ workinghelloworld.js
@@ -0,0 +1,12 @@
+var express = require('express')
+var app = express()
+var http = require ('http')
+// respond with "hello world" when a GET request is made to the homepage
+var server = http.createServer(function (request, response) {  
+  response.writeHead(200, {"Content-Type": "text/html"});
+
+app.get('/', function (req, res) {
+  res.send('hello world')
+})
+server.listen(9000)})
+
